d121f0f0a8f6c3c8f4d3817be9110bb40699def3
odom50 <kbibi0865@gmail.com>
2024-08-19 21:36:01 +0900

    Add deployment scripts and configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-// next.config.js
+  // next.config.js
 module.exports = {
-    output: 'export', // Ensure Next.js exports static HTML
+    output: 'export', // Ensures Next.js exports static HTML
     images: {
       unoptimized: true, // Disable image optimization for static export
     },
     trailingSlash: true, // Optional: Adds a trailing slash to URLs
   };
   
+
+
+  // /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
